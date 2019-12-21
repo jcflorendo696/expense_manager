6c9e89de23fc70e76f8a75f807125e3d6aeae668
Jc <jc.florendo696@gmail.com>
2019-12-21 08:40:12 +0800

    Updated superadmin seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,11 +16,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Super Admin',
+            'name' => 'Super_Admin',
             'email' => 'superadmin@gmail.com',
             'role' => 'admin',
             'password' => Hash::make('admin12345'),
-            ''
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
         ]);
     }
 }
